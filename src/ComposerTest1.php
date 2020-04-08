33767edb25b8232c7ff7b277fef489c95eedb069
gale boy <9983943198@qq.com>
2020-04-08 17:01:55 +0800

    upload composer/test

--- src/ComposerTest1.php
+++ src/ComposerTest1.php
@@ -10,5 +10,6 @@ class ComposerTest1
         echo '我总是这么帅' . PHP_EOL;
         echo "一次就好，我带你去看天荒地老" . PHP_EOL;
         echo "在阳光灿烂的日子里开怀大笑" . PHP_EOL;
+        echo "在自由自在的空气里吵吵闹闹" . PHP_EOL;
     }
 }
