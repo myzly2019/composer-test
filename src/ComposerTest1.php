3429c9c7a87cfbde7397f1de3370622ce30586a1
crabapple <2276447099@qq.com>
2020-04-08 16:09:28 +0800

    upload composer/test

--- src/ComposerTest1.php
+++ src/ComposerTest1.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Test;
+namespace ComposerTest;
 
 
-class Test
+class ComposerTest1
 {
     public function index()
     {
-        echo '我总是这么帅';
+        echo '我总是这么帅' . PHP_EOL;
         echo "一次就好，我带你去看天荒地老" . PHP_EOL;
     }
 }
