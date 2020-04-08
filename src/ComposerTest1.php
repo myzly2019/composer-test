af3821bf61dd58f9007f19987dce9444b428129a
gale boy <9983943198@qq.com>
2020-04-08 17:33:22 +0800

    upload composer/test

--- src/ComposerTest1.php
+++ src/ComposerTest1.php
@@ -11,5 +11,6 @@ class ComposerTest1
         echo "一次就好，我带你去看天荒地老" . PHP_EOL;
         echo "在阳光灿烂的日子里开怀大笑" . PHP_EOL;
         echo "在自由自在的空气里吵吵闹闹" . PHP_EOL;
+        echo "你可知道我唯一的想要" . PHP_EOL;
     }
 }
