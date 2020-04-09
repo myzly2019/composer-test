d4e38f680baa5e2edfdc89c711981c44420a147f
gale boy <9983943198@qq.com>
2020-04-09 11:47:11 +0800

    upload composer/test

--- src/Provider/ComposerTestServiceProvider.php
+++ src/Provider/ComposerTestServiceProvider.php
@@ -2,23 +2,36 @@
 
 namespace ComposerTest\Provider;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class ComposerTestServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
         $this->publishes([
-            __DIR__.'/../config/composer_test.php' => config_path('permission.php'),
+            __DIR__.'/../../config/composer_test.php' => config_path('composer_test.php'),
         ], 'config');
 
         $this->publishes([
-            __DIR__.'/../database/migrations/create_test_table.php' => database_path('migrations')
+            __DIR__.'/../../database/migrations/create_test_table.php' => $this->getMigrationFileName($filesystem)
         ], 'migrations');
     }
 
     public function register()
     {
 
+    }
+
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_test_table.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_test_table.php")
+            ->first();
     }
 }
